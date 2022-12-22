f85637d9f76a13e75149580184e0d725fe44b752
flare-s <webflarez@gmail.com>
2022-12-22 14:28:45 +0000

    Filtering todos by category

--- src/App.svelte
+++ src/App.svelte
@@ -20,11 +20,21 @@
 		isEditing: false}
 	]
 
-	$: uncompletedTodos = todos.filter(todo => !todo.isComplete).length
+
+	let currentFilter = "all"
+	$: 
+		filteredTodos = currentFilter === "all" ? todos 
+		: currentFilter === "active" 
+		? todos.filter(todo => !todo.isComplete) 
+		: todos.filter(todo => todo.isComplete)
+
 
 	const createId = () => {
 		return todos.length === 0 ? 1 : todos[todos.length - 1].id + 1
 	}
+
+	$: uncompletedTodos = todos.filter(todo => !todo.isComplete).length
+
 	let todoTitle = "";
 	const addNewTodo = () => {
 		todos = [...todos, {
@@ -64,56 +74,61 @@
 		  bind:value={todoTitle}
 		/>
 	  </form>
-  
-	  <ul class="todo-list">
-		{#each todos as todo}
-		<li class="todo-item-container {todo.id}" class:line-through={todo.isComplete}>
-			<div class="todo-item">
-			  <input type="checkbox" bind:checked={todo.isComplete}/>
-			  <span class="todo-item-label">{todo.title}</span>
-			  <!-- <input
-				type="text"
-				class="todo-item-input"
-				value="Finish Svelte Series"
-			  /> -->
+	  {#if todos.length > 0}
+		 <!-- content here -->
+		 <ul class="todo-list">
+			{#each filteredTodos as todo}
+			<li class="todo-item-container {todo.id}" class:line-through={todo.isComplete}>
+				<div class="todo-item">
+				  <input type="checkbox" bind:checked={todo.isComplete}/>
+				  <span class="todo-item-label">{todo.title}</span>
+				  <!-- <input
+					type="text"
+					class="todo-item-input"
+					value="Finish Svelte Series"
+				  /> -->
+				</div>
+				<button class="x-button" on:click={deleteTodo(todo.id)}>
+				  <svg
+					class="x-button-icon"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				  >
+					<path
+					  strokeLinecap="round"
+					  strokeLinejoin="round"
+					  strokeWidth={2}
+					  d="M6 18L18 6M6 6l12 12"
+					/>
+				  </svg>
+				</button>
+			  </li>
+			{/each}
+		  </ul>
+	  
+		  <div class="check-all-container">
+			<div>
+			  <button on:click={checkAllTodos} class="button">Check All</button>
 			</div>
-			<button class="x-button" on:click={deleteTodo(todo.id)}>
-			  <svg
-				class="x-button-icon"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			  >
-				<path
-				  strokeLinecap="round"
-				  strokeLinejoin="round"
-				  strokeWidth={2}
-				  d="M6 18L18 6M6 6l12 12"
-				/>
-			  </svg>
-			</button>
-		  </li>
-		{/each}
-	  </ul>
-  
-	  <div class="check-all-container">
-		<div>
-		  <button on:click={checkAllTodos} class="button">Check All</button>
-		</div>
-  
-		<span>{uncompletedTodos} items remaining</span>
-	  </div>
-  
-	  <div class="other-buttons-container">
-		<div>
-		  <button class="button filter-button filter-button-active"> All </button>
-		  <button class="button filter-button">Active</button>
-		  <button class="button filter-button">Completed</button>
-		</div>
-		<div>
-		  <button class="button" on:click={clearCompletedTodos}>Clear completed</button>
-		</div>
-	  </div>
+	  
+			<span>{uncompletedTodos} items remaining</span>
+		  </div>
+	  
+		  <div class="other-buttons-container">
+			<div>
+			  <button class="button filter-button" on:click={() => currentFilter = "all"} class:filter-button-active={currentFilter === "all"}> All </button>
+			  <button class="button filter-button" on:click={() => currentFilter = "active"} class:filter-button-active={currentFilter === "active"}>Active</button>
+			  <button class="button filter-button" on:click={() => currentFilter = "completed"} class:filter-button-active={currentFilter === "completed"}>Completed</button>
+			</div>
+			<div>
+			  <button class="button" on:click={clearCompletedTodos}>Clear completed</button>
+			</div>
+		  </div>
+	  {:else}
+		 <p class="todo-list-empty">You don't have any todos at the moment</p>
+	  {/if}
+	  
 	</div>
   </div>
 
