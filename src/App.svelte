281cbf5be993064da35fb4364219802ae3a63273
flare-s <webflarez@gmail.com>
2022-12-22 12:57:36 +0000

    Show todos dinamically

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,25 @@
 <script>
 	import "./css/reset.css";
 	import "./css/style.css";
+
+	let todos = [
+		{
+			id: 1,
+		title: "Finish Svelte course",
+		isComplete: false,
+		isEditing: false},
+		{
+			id: 2,
+		title: "Go for a run",
+		isComplete: false,
+		isEditing: false},
+		{
+		id: 3,
+		title: "Cook something to eat",
+		isComplete: false,
+		isEditing: false}
+	]
+
   </script>
   
   <div class="todo-app-container">
@@ -15,32 +34,34 @@
 	  </form>
   
 	  <ul class="todo-list">
+		{#each todos as todo}
 		<li class="todo-item-container">
-		  <div class="todo-item">
-			<input type="checkbox" />
-			<span class="todo-item-label">Finish Svelte Series</span>
-			<!-- <input
-			  type="text"
-			  class="todo-item-input"
-			  value="Finish Svelte Series"
-			/> -->
-		  </div>
-		  <button class="x-button">
-			<svg
-			  class="x-button-icon"
-			  fill="none"
-			  viewBox="0 0 24 24"
-			  stroke="currentColor"
-			>
-			  <path
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth={2}
-				d="M6 18L18 6M6 6l12 12"
-			  />
-			</svg>
-		  </button>
-		</li>
+			<div class="todo-item">
+			  <input type="checkbox" />
+			  <span class="todo-item-label">{todo.title}</span>
+			  <!-- <input
+				type="text"
+				class="todo-item-input"
+				value="Finish Svelte Series"
+			  /> -->
+			</div>
+			<button class="x-button">
+			  <svg
+				class="x-button-icon"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			  >
+				<path
+				  strokeLinecap="round"
+				  strokeLinejoin="round"
+				  strokeWidth={2}
+				  d="M6 18L18 6M6 6l12 12"
+				/>
+			  </svg>
+			</button>
+		  </li>
+		{/each}
 	  </ul>
   
 	  <div class="check-all-container">
